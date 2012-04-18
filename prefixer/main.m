4bb0b998804194e46d64b69a0c1e30a11b0accfc
Twan Wolthof <twan@xeago.nl>
2012-04-18 15:54:15 +0200

    Initial Commit

---
+++ prefixer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prefixer
+//
+//  Created by Twan Wolthof on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
